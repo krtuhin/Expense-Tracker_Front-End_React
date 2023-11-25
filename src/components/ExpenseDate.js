9660975ff8abc79cfe5d2b711b0d069176e2cfcd
krtuhin <tuhin.km2774@gmail.com>
2023-11-25 22:09:58 +0530

    Break component into various sub components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,23 @@
+// import css
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+
+  // modify props object data to use in JSX
+  let day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  let month = props.date.toLocaleString("en-US", { month: "long" });
+  let year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+        
+      {/* used modified data */}
+      <div className="expense-date_day">{day}</div>
+      <div className="expense-date_month">{month}</div>
+      <div className="expense-date_year">{year}</div>
+    </div>
+  );
+}
+
+// export component
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,17 @@
 // import sub component's css
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+
   //declare variable in component
   //   let title = "Car Insurence";
 
-  // modify props object data to use in JSX
-  let day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  let month = props.date.toLocaleString("en-US", { month: "long" });
-  let year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div className="expense-date">
-        {/* used modified data */}
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      
+      {/* using expense date sub component */}
+      <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
         {/* using JS data into JSX */}

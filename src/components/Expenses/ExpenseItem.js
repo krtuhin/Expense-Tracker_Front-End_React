8dcc4be0f43bac081f721e5951fcd418aab0296d
krtuhin <tuhin.km2774@gmail.com>
2023-11-26 02:42:08 +0530

    State in React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
-// import react
-import React from "react";
+// import react and react hook (use state)
+import React, { useState } from "react";
 
 // import sub component's css
 import "./ExpenseItem.css";
@@ -7,9 +7,15 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  
-  //declare variable in component
-  //   let title = "Car Insurence";
+
+  /* destructuring the array with two variable one denoting the value that is 
+  passed and another is the method to change the passed value */
+  const [title, setTitle] = useState(props.title);
+
+  // creating function clickhandler
+  let clickHandler = () => {
+    setTitle("New Title");
+  };
 
   return (
     // using component as a parent tag
@@ -20,10 +26,13 @@ const ExpenseItem = (props) => {
 
       <div className="expense-item__description">
         {/* using JS data into JSX */}
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
 
         <div className="expense-item__price">$ {props.price}</div>
       </div>
+
+      {/* using click event (use function in the click event without '()') */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };

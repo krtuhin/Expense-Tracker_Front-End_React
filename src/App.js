8cc16b068ec01819c3b07e892c167c323171bbb3
krtuhin <tuhin.km2774@gmail.com>
2023-12-20 03:46:51 +0530

    Send Data to BackEnd (POST) using Fetch API and Rerander Page with Updated Data

--- src/App.js
+++ src/App.js
@@ -18,20 +18,41 @@ const App = () => {
   // creating updatable expense list using useState
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  // useEffect hook to avoid infinite looping
-  useEffect(() => {
-    // fetching data from API
-    fetch("http://127.0.0.1:8080/all-expenses")
+  // function for fetch API
+  let fetchData = () => {
+    fetch("http://127.0.0.1:8080/all-items")
       .then((response) => response.json())
       .then((data) => setExpenses(data))
       .catch((error) => console.log(error));
+  };
+
+  // useEffect hook to avoid infinite looping
+  useEffect(() => {
+    // fetching data from API
+    fetchData();
   }, []);
 
   // getting child's data as function parameter
   const addExpenseHandler = (expense) => {
     // updating expense list
-    const updatedExpenses = [expense, ...expenses];
-    setExpenses(updatedExpenses);
+    // const updatedExpenses = [expense, ...expenses];
+    // setExpenses(updatedExpenses);
+
+    // sending data to backend using fetch API
+    fetch("http://127.0.0.1:8080/save-item", {
+      method: "POST",
+      body: JSON.stringify(expense),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        // rerander page with updated data
+        fetchData();
+        return response.text();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
   };
 
   return (
